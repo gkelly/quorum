4fece5e26bdaaf8f8d5a158325f5990664c18711
Garret Kelly <gkelly@gkel.ly>
2022-11-26 18:14:27 -0500

    quorum: Pre-allocate vector of appropriate size

--- src/quorum.rs
+++ src/quorum.rs
@@ -40,7 +40,7 @@ pub(crate) fn generate(args: &Generate) -> Result<()> {
 }
 
 pub(crate) fn recover_secret(share_paths: Vec<String>, threshold: u8) -> Result<[u8; 32]> {
-    let mut shares: Vec<Share> = vec![];
+    let mut shares = Vec::with_capacity(share_paths.len());
     let sharks = Sharks(threshold);
 
     for path in share_paths {
